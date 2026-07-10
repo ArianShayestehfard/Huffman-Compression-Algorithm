432a927677681867ad1fd13a6707b1d1b89ff453
Arian Shayestehfard <arian.shayestehfard@gmail.com>
2026-07-10 15:42:36 +0330

    Main Update!

--- Main.py
+++ Main.py
@@ -3,3 +3,11 @@ import pickle
 import os
 from collections import Counter
 
+class Node:
+    def __init__(self, char=None, freq=0, left=None, right=None):
+        self.char = char
+        self.freq = freq
+        self.left = left
+        self.right = right
+    def __lt__(self, other):
+        return self.freq < other.freq
